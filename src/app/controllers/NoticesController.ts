74592336bd14f98d5130fd06583676ceb4c4b818
Sw1tlana <127736251+Sw1tlana@users.noreply.github.com>
2025-03-07 13:05:35 +0200

    add lean()

--- src/app/controllers/NoticesController.ts
+++ src/app/controllers/NoticesController.ts
@@ -1,14 +1,14 @@
 import { Notice } from "app/domain/models/Notices.model";
 import { ApiError } from "helpers/ApiError";
 import { ApiResponse } from "helpers/ApiResponse";
-import { JsonController, Get, Post, Body, Authorized, Param } from "routing-controllers";
+import { JsonController, Get, Post, Body, Authorized, Param, QueryParam } from "routing-controllers";
 
 @JsonController("/notices")
 export class NoticesController {
     @Get("/")
     async getNotices() {
         try {
-        const notices = await Notice.find();
+        const notices = await Notice.find().lean();
         return new ApiResponse(true, notices);
         }catch(error) {
             return new ApiError(400, { message: "Validation failed"});
@@ -32,7 +32,7 @@ async getNotiesById(@Param('id') id: string) {
 @Get("/categories")
 async getCategories() {
     try {
-        const categories = await Notice.distinct("category");
+        const categories = await Notice.distinct("category").lean();
         return new ApiResponse(true, categories);
     }catch(error) {
         return new ApiError(400, { message: "Validation failed"});
@@ -42,9 +42,14 @@ async getCategories() {
 @Get("/sex")
 async getSex() {
     try {
-        const sex = await Notice.distinct("sex");
-        return new ApiResponse(true, sex);
+        const notices = await Notice.distinct("sex").lean();
+        if (!notices || notices.length === 0) {
+            return new ApiError(404, { message: "No notices found" });
+        }
+
+        return new ApiResponse(true, notices);
     }catch(error) {
+        console.error("Error fetching sex notices:", error); 
         return new ApiError(400, { message: "Validation failed"});
     }
 }
